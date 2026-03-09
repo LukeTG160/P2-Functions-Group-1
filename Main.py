d2838616fc32a0c5b8f51c839b3275022da37de0
brycegum123 <bryceflindt@gmail.com>
2026-03-09 17:10:05 -0600

    Intro/Menu
    Added the intro function and menu to the main

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
+# Import functions from Soccer.py
+from Soccer import introduction, display_menu
 
+def main():
+
+    # Call introduction function (Bryce)
+    sPlayerName = introduction()
+
+    # Call menu function (Bryce)
+    sChoice = display_menu()
+
+    # Basic menu logic
+    if sChoice == "1":
+        print("Starting the season...")
+
+    elif sChoice == "2":
+        print("Displaying record...")
+
+    elif sChoice == "3":
+        print("Goodbye " + sPlayerName)
+
+
+# Run the program
+main()
