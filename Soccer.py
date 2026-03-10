2374469f860e8c0b9378be3bdd9623a2439d5bea
brycegum123 <bryceflindt@gmail.com>
2026-03-09 19:07:48 -0600

    Update Soccer.py

--- Soccer.py
+++ Soccer.py
@@ -44,3 +44,24 @@ def display_menu():
 # sChoice = display_menu()
 # print("Player name is:", sPlayerName)
 # print("Menu choice is:", sChoice)
+def display_record(sHomeTeam, iWins, iLosses):
+
+    print()
+    print("Season Record")
+    print("----------------------")
+    print("Team:", sHomeTeam)
+
+    # Calculate total games played
+    iTotalGames = iWins + iLosses
+
+    print("Games Played:", iTotalGames)
+    print("Wins:", iWins)
+    print("Losses:", iLosses)
+
+    # Calculate win percentage
+    if iTotalGames > 0:
+        fWinPercent = iWins / iTotalGames
+        print("Win Percentage:", round(fWinPercent, 2))
+
+    print("----------------------")
+    print()
