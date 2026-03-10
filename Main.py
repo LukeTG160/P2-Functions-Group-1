b1a039f67f93e90005ffe54132e1d9e537bd9751
LukeTG160 <luketgreen919@gmail.com>
2026-03-10 00:46:21 -0600

    Edited Step5Functions

--- Main.py
+++ Main.py
@@ -1,5 +1,24 @@
 # Import functions from Soccer.py
 from Soccer import introduction, display_menu, display_record
+import random
+
+def play_game(sHomeTeam, sOpponentTeam):
+    iHomeScore = random.randint(0, 5)
+    iOpponentScore = random.randint(0, 5)
+    
+    while iHomeScore == iOpponentScore:
+        iOpponentScore = random.randint(0, 5)
+    
+    print()
+    print(f"{sHomeTeam} vs {sOpponentTeam}")
+    print(f"Final Score: {sHomeTeam} {iHomeScore} - {iOpponentScore} {sOpponentTeam}")
+    
+    if iHomeScore > iOpponentScore:
+        print(f"{sHomeTeam} wins!")
+        return "W"
+    else:
+        print(f"{sOpponentTeam} wins!")
+        return "L"
 
 def main():
 
